e1f49a43d218449c059accc8a3fa38daadffbffd
Damnun <shmom467@hanmail.net>
2023-10-20 05:30:18 +0900

    [FEAT] : template rendering 추가
    [FEAT] : template rendering 추가

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+import stream
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("landing.html")
+
+
+@app.route('/landing2')
+def landing2():
+    return render_template("landing2.html")
+
+
+@app.route('/landing3')
+def landing3():
+    return render_template("landing3.html")
+
+
+@app.route('/doctor-login')
+def doctor_login():
+    return render_template("doctor-login.html")
+
+
+@app.route('/doctor-register')
+def doctor_register():
+    return render_template("doctor-register.html")
+
+
+@app.route('/contact')
+def contact():
+    return "contact는 준비중";
+
+@app.route("/user-precheck")
+def user_precheck():
+    return render_template("user-precheck.html")
+
+
+@app.route("/user-camera-check")
+def user_camera_check():
+    return Response(stream.GenerateFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
---
+++ stream.py
@@ -0,0 +1,19 @@
+from time import sleep
+import cv2
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+def GenerateFrames():
+    while True:
+        # sleep(0.1)
+        ref, frame = capture.read()
+        if not ref:
+            break
+        else:
+            ref, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
